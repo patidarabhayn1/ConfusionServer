a3baa67eb9b26c054dea4e3cf5ee9481bb6a5a4a
Abhay Patidar <patidarabhayn1@gmail.com>
2020-11-30 10:22:18 +0530

    Facebook Authentication OAuth 2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '757992004792920',
+        clientSecret: 'dde9de2f3ad0533e7020c8086b5e8738'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -53,7 +53,7 @@ router.post('/signup', cors.corsWithOptions , (req, res, next) => {
 
 router.post('/login',cors.corsWithOptions , passport.authenticate('local'), (req, res) => {
 
-    var token = authenticate.getToken({_id: req.user._id})
+    var token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json({success: true, token: token, status: 'You are Successfully Logged In'});
@@ -69,6 +69,15 @@ router.get('/logout', (req, res, next) => {
     var err = new Error('You are not logged in!');
     err.status = 403;
     next(err);
+  }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if(req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are Successfully Logged In'});
   }
 });
 
